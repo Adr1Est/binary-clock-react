8aca26bb2036e81ae8c05fb14205bf891d606039
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2025-05-12 13:04:44 +0200

    cambio de color texto botones

--- src/layout/buttons/buttons-layout.jsx
+++ src/layout/buttons/buttons-layout.jsx
@@ -3,8 +3,8 @@ function ButtonsApp({funcionStart, funcionStop}){
   return (
     <>
       <div className="flex flex-row justify-center items-center gap-5 w-full p-4">
-        <button className="bg-gray-800 hover:bg-gray-500 text-amber-300 hover:text-green-500 font-bold w-3/5 md:w-2/5 rounded-xl p-3" onClick={funcionStart}>Start</button>
-        <button className="bg-gray-800 hover:bg-gray-500 text-amber-300 hover:text-red-500 font-bold w-3/5 md:w-2/5 rounded-xl p-3" onClick={funcionStop}>Stop</button>
+        <button className="bg-gray-800 hover:bg-gray-500 text-gray-300 hover:text-green-500 font-bold w-3/5 md:w-2/5 rounded-xl p-3" onClick={funcionStart}>Start</button>
+        <button className="bg-gray-800 hover:bg-gray-500 text-gray-300 hover:text-red-500 font-bold w-3/5 md:w-2/5 rounded-xl p-3" onClick={funcionStop}>Stop</button>
       </div>
     </>
   )
