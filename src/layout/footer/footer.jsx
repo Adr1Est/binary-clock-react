fd2c2074a705259e93c9b8221aea42394c2d970b
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2025-05-13 20:57:37 +0200

    icono con enlace al repositorio y usuario de github

--- src/layout/footer/footer.jsx
+++ src/layout/footer/footer.jsx
@@ -1,10 +1,18 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+
 function Footer(){
 
   return (
     <>
       <div className="flex flex-row justify-center items-center text-gray-300 w-full">
-        
-        <p>&copy; Adr1Est</p>
+        <p className='font-semibold opacity-30'>&copy; Adr1Est |</p>
+        <a href="https://github.com/Adr1Est/binary-clock-react" target='_blank'>
+          <FontAwesomeIcon 
+            className='ml-1 opacity-30 hover:text-amber-400 hover:opacity-100' 
+            icon={faGithub} 
+          />
+        </a>
       </div>
     </>
   )
